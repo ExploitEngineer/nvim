1bbba673701fe0440308dd39d7bfc4cce4eb99e1
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-04 19:21:51 +0500

    feat: add configured bufferline plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,7 +24,7 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
-keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "te", ":tabedit<CR>", opts)
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -21,6 +21,22 @@ return {
     },
   },
 
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
   -- statusline
   {
     "nvim-lualine/lualine.nvim",
