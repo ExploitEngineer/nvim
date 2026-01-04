27230c3ed624e3677e9702f92cee21dc382eff86
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-05 01:00:31 +0500

    feat: add kulala plugin for api testing in neovim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -212,4 +212,100 @@ return {
     opts = {},
     cmd = { "Typr", "TyprStats" },
   },
+
+  -- kulala for api testing
+  {
+    "mistweaverco/kulala.nvim",
+    ft = "http", -- load only for .http files
+    opts = {
+      global_keymaps = true, -- enable built-in defaults
+      global_keymaps_prefix = "<leader>R", -- group under <leader>R
+    },
+    keys = {
+      {
+        "<leader>Rb",
+        function()
+          require("kulala").scratchpad()
+        end,
+        desc = "Open scratchpad",
+      },
+      {
+        "<leader>Rc",
+        function()
+          require("kulala").copy()
+        end,
+        desc = "Copy as cURL",
+      },
+      {
+        "<leader>RC",
+        function()
+          require("kulala").from_curl()
+        end,
+        desc = "Paste from cURL",
+      },
+      {
+        "<leader>Rg",
+        function()
+          require("kulala").download_graphql_schema()
+        end,
+        desc = "Download GraphQL schema",
+      },
+      {
+        "<leader>Ri",
+        function()
+          require("kulala").inspect()
+        end,
+        desc = "Inspect current request",
+      },
+      {
+        "<leader>Rn",
+        function()
+          require("kulala").jump_next()
+        end,
+        desc = "Jump to next request",
+      },
+      {
+        "<leader>Rp",
+        function()
+          require("kulala").jump_prev()
+        end,
+        desc = "Jump to previous request",
+      },
+      {
+        "<leader>Rq",
+        function()
+          require("kulala").close()
+        end,
+        desc = "Close window",
+      },
+      {
+        "<leader>Rr",
+        function()
+          require("kulala").replay()
+        end,
+        desc = "Replay last request",
+      },
+      {
+        "<leader>Rs",
+        function()
+          require("kulala").run()
+        end,
+        desc = "Send the request",
+      },
+      {
+        "<leader>RS",
+        function()
+          require("kulala").show_stats()
+        end,
+        desc = "Show stats",
+      },
+      {
+        "<leader>Rt",
+        function()
+          require("kulala").toggle_view()
+        end,
+        desc = "Toggle headers/body",
+      },
+    },
+  },
 }
