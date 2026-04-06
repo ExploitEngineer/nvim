80580a05a783c3d92377622bf85823faf1d0951c
whoami <abdulrafayofficial.work@gmail.com>
2026-04-06 14:15:25 +0500

    Restore Telescope startup by adding fzf-native dependency

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,182 +1,129 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-file-browser.nvim",
-  },
-  keys = {
-    {
-      "<leader>fP",
-      function()
-        require("telescope.builtin").find_files({
-          cwd = require("lazy.core.config").options.root,
-        })
-      end,
-      desc = "Find Plugin File",
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-    {
-      "<leader>ff",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.find_files({
-          no_ignore = false,
-          hidden = true,
-        })
-      end,
-      desc = "Find Files",
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files({ hidden = true })
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Live Grep",
+      },
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        desc = "Help Tags",
+      },
+      {
+        "<leader>fr",
+        function()
+          require("telescope.builtin").resume()
+        end,
+        desc = "Resume Telescope",
+      },
+      {
+        "<leader>fd",
+        function()
+          require("telescope.builtin").diagnostics()
+        end,
+        desc = "Diagnostics Picker",
+      },
+      {
+        "<leader>fe",
+        function()
+          require("telescope").extensions.file_browser.file_browser({
+            path = vim.fn.expand("%:p:h"),
+            select_buffer = true,
+          })
+        end,
+        desc = "File Browser",
+      },
     },
-    {
-      "<leader>fg",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.live_grep()
-      end,
-      desc = "Live Grep",
-    },
-    {
-      "\\\\",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.buffers()
-      end,
-    },
-    {
-      "<leader>fh",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.help_tags()
-      end,
-      desc = "Help Tags",
-    },
-    {
-      "<leader>fr",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.resume()
-      end,
-      desc = "Resume Telescope",
-    },
-    {
-      "<leader>fd",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.diagnostics()
-      end,
-      desc = "Diagnostics Picker",
-    },
-    {
-      "<leader>fs",
-      function()
-        local builtin = require("telescope.builtin")
-        builtin.treesitter()
-      end,
-      desc = "Treesitter Symbols",
-    },
-    {
-      "sf",
-      function()
-        local telescope = require("telescope")
-        local function telescope_buffer_dir()
-          return vim.fn.expand("%:p:h")
-        end
+    opts = function(_, opts)
+      local actions = require("telescope.actions")
+      local fb_actions = require("telescope").extensions.file_browser.actions
 
-        telescope.extensions.file_browser.file_browser({
-          path = telescope_buffer_dir(),
-          cmd = telescope_buffer_dir(),
-          respect_gitignore = false,
-          hidden = true,
-          grouped = false,
-          previewer = false,
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      })
+
+      opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
+        diagnostics = {
+          theme = "ivy",
           initial_mode = "normal",
-          layout_config = { height = 40 },
-        })
-      end,
-    },
-  },
-  config = function(_, opts)
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local fb_actions = require("telescope").extensions.file_browser.actions
-
-    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-      wrap_result = true,
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      winblend = 0,
-      mappings = {
-        n = {},
-      },
-    })
-    opts.pickers = {
-      diagnostics = {
-        theme = "ivy",
-        initial_mode = "normal",
-        layout_config = {
-          preview_cutoff = 9999,
+          layout_config = { preview_cutoff = 9999 },
         },
-      },
-    }
-    opts.extensions = {
-      file_browser = {
-        theme = "dropdown",
-        -- disables netw and use telescope-file-browser in its place
-        hijack_netrw = true,
-        mappings = {
-          ["n"] = {
-            -- your custom normal mode mappings
-            ["N"] = fb_actions.create,
-            ["h"] = fb_actions.goto_parent_dir,
-            ["/"] = function()
-              vim.cmd("startinsert")
-            end,
-            ["<C-u>"] = function(prompt_bufnr)
-              for i = 1, 10 do
-                actions.move_selection_previous(prompt_bufnr)
-              end
-            end,
-            ["<C-d>"] = function(prompt_bufnr)
-              for i = 1, 10 do
-                actions.move_selection_next(prompt_bufnr)
-              end
-            end,
-            ["<PageUp>"] = actions.preview_scrolling_up,
-            ["<PageDown>"] = actions.preview_scrolling_down,
+      })
+
+      opts.extensions = vim.tbl_deep_extend("force", opts.extensions or {}, {
+        file_browser = {
+          theme = "dropdown",
+          hijack_netrw = true,
+          mappings = {
+            ["n"] = {
+              ["N"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
+            },
           },
         },
-      },
-    }
-    telescope.setup(opts)
-    require("telescope").load_extension("fzf")
-    require("telescope").load_extension("file_browser")
-  end,
+      })
+    end,
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+      telescope.load_extension("file_browser")
+    end,
+  },
+
   {
     "folke/flash.nvim",
     opts = {
       modes = {
         char = {
-          -- Keep native `;` and `,` repeat behavior for f/F/t/T.
           keys = { "f", "F", "t", "T" },
           jump_labels = true,
         },
       },
-      highlight = {
-        backdrop = true,
-      },
     },
   },
 
-  -- animations
   {
     "nvim-mini/mini.animate",
     event = "VeryLazy",
     opts = function(_, opts)
-      opts.scroll = {
-        enable = false,
-      }
+      opts.scroll = { enable = false }
     end,
   },
 
-  -- logo
   {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
@@ -186,18 +133,18 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local logo = [[
-                         .__         
-  ____   ____  _______  _|__| _____  
- /    \_/ __ \/  _ \  \/ /  |/     \ 
+                         .__
+  ____   ____  _______  _|__| _____
+ /    \_/ __ \/  _ \  \/ /  |/     \
 |   |  \  ___(  <_> )   /|  |  Y Y  \
 |___|  /\___  >____/ \_/ |__|__|_|  /
-     \/     \/                    \/ 
+     \/     \/                    \/
       ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
 
       require("dashboard").setup({
-        theme = "hyper", --or "doom"
+        theme = "hyper",
         config = {
           header = vim.split(logo, "\n"),
         },
@@ -205,136 +152,26 @@ return {
     end,
   },
 
-  -- wakatime
-  {
-    "wakatime/vim-wakatime",
-    lazy = false,
-  },
-
-  -- Typr plguin for typing inside neovim
-  {
-    "nvzone/typr",
-    dependencies = "nvzone/volt",
-    opts = {},
-    cmd = { "Typr", "TyprStats" },
-  },
-
-  -- kulala for api testing
-  {
-    "mistweaverco/kulala.nvim",
-    ft = "http", -- load only for .http files
-    opts = {
-      global_keymaps = true, -- enable built-in defaults
-      global_keymaps_prefix = "<leader>R", -- group under <leader>R
-    },
-    keys = {
-      {
-        "<leader>Rb",
-        function()
-          require("kulala").scratchpad()
-        end,
-        desc = "Open scratchpad",
-      },
-      {
-        "<leader>Rc",
-        function()
-          require("kulala").copy()
-        end,
-        desc = "Copy as cURL",
-      },
-      {
-        "<leader>RC",
-        function()
-          require("kulala").from_curl()
-        end,
-        desc = "Paste from cURL",
-      },
-      {
-        "<leader>Rg",
-        function()
-          require("kulala").download_graphql_schema()
-        end,
-        desc = "Download GraphQL schema",
-      },
-      {
-        "<leader>Ri",
-        function()
-          require("kulala").inspect()
-        end,
-        desc = "Inspect current request",
-      },
-      {
-        "<leader>Rn",
-        function()
-          require("kulala").jump_next()
-        end,
-        desc = "Jump to next request",
-      },
-      {
-        "<leader>Rp",
-        function()
-          require("kulala").jump_prev()
-        end,
-        desc = "Jump to previous request",
-      },
-      {
-        "<leader>Rq",
-        function()
-          require("kulala").close()
-        end,
-        desc = "Close window",
-      },
-      {
-        "<leader>Rr",
-        function()
-          require("kulala").replay()
-        end,
-        desc = "Replay last request",
-      },
-      {
-        "<leader>Rs",
-        function()
-          require("kulala").run()
-        end,
-        desc = "Send the request",
-      },
-      {
-        "<leader>RS",
-        function()
-          require("kulala").show_stats()
-        end,
-        desc = "Show stats",
-      },
-      {
-        "<leader>Rt",
-        function()
-          require("kulala").toggle_view()
-        end,
-        desc = "Toggle headers/body",
-      },
-    },
-  },
-
-  -- neo-tree
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      log_level = "fatal",
-
+      close_if_last_window = true,
+      popup_border_style = "rounded",
       sources = { "filesystem", "buffers", "git_status" },
-
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
-
       window = {
         mappings = {
           ["l"] = "open",
           ["h"] = "close_node",
           ["<space>"] = "none",
-
           ["Y"] = {
             function(state)
               local node = state.tree:get_node()
@@ -342,29 +179,20 @@ return {
             end,
             desc = "Copy Path to Clipboard",
           },
-
           ["O"] = {
             function(state)
               require("lazy.util").open(state.tree:get_node().path, { system = true })
             end,
             desc = "Open with System Application",
           },
-
           ["P"] = { "toggle_preview", config = { use_float = false } },
         },
       },
-
       default_component_configs = {
         indent = {
           with_expanders = true,
           expander_collapsed = "",
           expander_expanded = "",
-        },
-        git_status = {
-          symbols = {
-            unstaged = "󰄱",
-            staged = "󰱒",
-          },
         },
       },
     },
